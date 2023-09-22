df4c4c99098200f6f16ce4d5ca187eeba7893c41
ashraf <ashrafabusunaina1986>
2023-09-22 14:26:55 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express=require('express')
+const app=express()
+
+const cors=require('cors')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+
+app.route('/').get((req,res)=>{
+    res.json({meassge:"HELLO WORLD"})
+})
+
+
+const port=process.env.PORT || 4011
+app.listen(port,function (err) {
+    if(err) console.log('Error server ssetup')
+    console.log(`start server http://localhost:${port}`)
+})
+
+
+
