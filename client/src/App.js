c250de32c1a94b63d9be3d4aa1432d12f2509874
ashraf <ashrafabusunaina1986>
2023-09-22 18:21:41 +0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button';
+import Input from './components/Input/Input';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Input title='Name' input={{
+        id:'name',type:'text',placeholder:'enter the name.'
+      }}/>
+      <Button>Click for send request</Button>
     </div>
   );
 }
