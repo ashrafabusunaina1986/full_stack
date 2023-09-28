031660b4356b9965e787ffc4e1f7e1ead73b97f4
ashraf <ashrafabusunaina1986>
2023-09-28 11:00:52 +0300

    first commit

--- client/src/costum-hook/useform/use-form.js
+++ client/src/costum-hook/useform/use-form.js
@@ -16,19 +16,98 @@ const useForm = (elements) => {
         })
         return variavles
     }
-    const [entry, setEntry] = useState(init)
-    const [errorValue, setErrorValue] = useState(init(true))
+    const [entry, setEntry] = useState(init(true))
+    const [errorValue, setErrorValue] = useState(init)
 
+    const signvalue = e => {
+        const error = {}
+        const { id, value, type, nodeName } = e.target
+        if (nodeName === 'INPUT') {
+            if (type === 'number') {
+
+                if (value < 1) {
+                    setErrorValue({
+                        ...errorValue, [id]: `please enter ${id} >0`
+                    })
+                    setEntry({
+                        ...entry, [id]: 1
+                    })
+                } else {
+                    setErrorValue({
+                        ...errorValue, [id]: ``
+                    })
+                    setEntry({
+                        ...entry, [id]: Number(value)
+                    })
+                }
+            }
+            if (type === 'text') {
+
+                if (value.trim().length < 3) {
+                    setErrorValue({
+                        ...errorValue, [id]: `please enter ${id} > 2 letter`
+                    })
+                    setEntry({
+                        ...entry, [id]: ''
+                    })
+                } else {
+                    setErrorValue({
+                        ...errorValue, [id]: ``
+                    })
+                    setEntry({
+                        ...entry, [id]: value
+                    })
+                }
+            }
+        }
+        if (nodeName === 'TEXTAREA') {
+
+            if (value.trim().length < 20) {
+                setErrorValue({
+                    ...errorValue, [id]: `please enter ${id} > 19 letter`
+                })
+                setEntry({
+                    ...entry, [id]: ''
+                })
+            } else {
+                setErrorValue({
+                    ...errorValue, [id]: ``
+                })
+                setEntry({
+                    ...entry, [id]: value
+                })
+            }
+        }
+    }
+
+    const sendSubmitHandler = e => {
+        e.preventDefault()
+        let numError=0
+        Object.keys(errorValue).map(error=>{
+            console.log(error)
+            if(errorValue[error].trim().length>0){
+                numError+=1
+            }
+        })
+        console.log(numError)
+        if(numError>0){
+            console.log('found errors')
+        }else{
+            console.log(entry, errorValue)
+        }
+        
+    }
     const form = () => {
-        return <Fragment>
-            {!elements && <div></div> }
+        return <form onSubmit={sendSubmitHandler}>
+            {!elements && <div></div>}
             {elements && elements.map((element, ind) => {
                 if (element.name === 'input') {
                     return <Fragment key={ind}>
                         <Input title={element.title}
                             input={{
                                 id: element.id,
-                                type: element.type
+                                type: element.type,
+                                onChange: (e) => signvalue(e)
                             }} />
                     </Fragment>
                 }
@@ -36,7 +115,8 @@ const useForm = (elements) => {
                     return <Fragment key={ind}>
                         <Textarea title={element.title}
                             input={{
-                                id: element.id
+                                id: element.id,
+                                onChange: (e) => signvalue(e)
                             }}
                         />
                     </Fragment>
@@ -44,12 +124,12 @@ const useForm = (elements) => {
                 if (element.name === "button") {
                     return <Fragment key={ind}>
                         <Button input={{
-                            id:element.id
+                            id: element.id
                         }} >{element.children}</Button>
                     </Fragment>
                 }
             })}
-        </Fragment>
+        </form>
     }
     return { errorValue, entry, form }
 }
